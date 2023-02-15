58dc606895af9780c6edbb287b49cdaa7b13fcd0
Juan Rodriguez <juan>
2023-02-15 19:08:24 +0100

    SECOND COMMIT DEPLOY MAIN db deploy datos

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
   password: process.env.DB_PASSWORD || "123456789",
   database: process.env.DB_NAME || "unbardo_design",
   host: process.env.DB_HOST || "localhost",
-  db_deploy: process.env.DB_DEPLOY || "postgresql://postgres:juGAIQLTDZyxiFUUIkQ9@containers-us-west-30.railway.app:5800/railway",
+  db_deploy: process.env.DB_DEPLOY || "postgresql://postgres:ar1cxwVG4hcZhPtpSunm@containers-us-west-55.railway.app:6434/railway",
   dialect: "postgres",
   define: {
     timestamps: true,
