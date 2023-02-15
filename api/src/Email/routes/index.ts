f0409d68dc0e0922ca56fb46d62be92305d2feac
Juan Jose Rodriguez <65400450+juancts@users.noreply.github.com>
2023-02-15 18:11:46 +0100

    Delete api directory

--- api/src/Email/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import sendEmailController from '../controllers';
-
-const emailRoutes = express.Router();
-
-emailRoutes.post('/', sendEmailController);
-
-export default emailRoutes;
--- api/src/helpers/handleErrorCatch.ts
+++
@@ -1,7 +0,0 @@
-export default function getErrorMessage (error?: unknown, message?: string) {
-    if(message) return {message: message}
-    if (error instanceof Error) return {message: error.message}
-
-    return {message: String(error)}
-  }
-  
